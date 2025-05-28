d1e0c4d1eab30f97135ee5de921d6c8c1cab880b
raphaelrdsilva <92390647+raphaelrdsilva@users.noreply.github.com>
2025-05-28 22:53:21 +0000

    commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,15 +3,15 @@
 
 int main () {
 
+    //INSERÇÃO DE DADOS DA PRIMEIRA CARTA
     char estado[5];
     char codcarta[5];
     char nome[25];
-    int populacao;
+    unsigned long int populacao;
     float area;
     float pib;
     int numponturi;
 
-    //Inserção de dados da Carta 1
     printf("Carta 1 \n");
 
     printf("Estado: ");
@@ -26,7 +26,7 @@ int main () {
     nome[strcspn(nome, "\n")] = 0;
 
     printf("População: ");
-    scanf("%d", &populacao);
+    scanf("%lu", &populacao);
 
     printf("Área: ");
     scanf("%f", &area);
@@ -48,16 +48,17 @@ int main () {
 
     float pibpercap1 = pib / populacao;
     float densidade1 = populacao / area;
+    float superpoder1 = populacao + area + pib + pibpercap1 + densidade1 + numponturi;
 
-    printf("PIB per capta: %.2f reais \n", pibpercap1);
-    printf("Densidade populacional(Km²): %.2f Hab/Km² \n", densidade1);
+    printf("PIB per capta: %.2f \n", pibpercap1);
+    printf("Densidade populacional(Km²): %.2f \n", densidade1);
+    printf("Super Poder: %.2f \n", superpoder1);
 
-//Inserção de dados da Carta 2
-    
+    //INSERÇÃO DE DADOS DA SEGUNDA CARTA
     char estado2[5];
     char codcarta2[5];
     char nome2[25];
-    int populacao2;
+    unsigned long int populacao2;
     float area2;
     float pib2;
     int numponturi2;
@@ -76,7 +77,7 @@ int main () {
     nome2[strcspn(nome2, "\n")] = 0;
 
     printf("População: ");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
 
     printf("Área: ");
     scanf("%f", &area2);
@@ -98,9 +99,30 @@ int main () {
 
     float pibpercap2 = pib2 / populacao2;
     float densidade2 = populacao2 / area2;
+    float superpoder2 = populacao2 + area2 + pib2 + pibpercap2 + densidade2 + numponturi2;
 
-    printf("PIB per Capta: %.2f reais \n", pibpercap2);
-    printf("Densidade populacional(Km²): %.2f hab/Km² \n", densidade2);
+    printf("Super Poder: %.2f \n", superpoder2);
+    printf("PIB per capta: %.2f \n", pibpercap2);
+    printf("Densidade populacional(Km²): %.2f \n", densidade2);
+    printf("Super Poder:%d", superpoder2);
+
+    //COMPARAÇÃO DE CARTAS
+    int compPopulacao = populacao > populacao2;
+    int compArea = area > area2;
+    int compPIB = pib > pib2;
+    int compPonturi = numponturi > numponturi2;
+    int compDensidade = densidade1 > densidade2;
+    int compPIBpercapta = pibpercap1 > pibpercap2;
+    int compSuperPoder = superpoder1 > superpoder2;
+
+    printf("Comparação de Cartas \n");
+    printf("População: CARTA 1 VENCEU? %d \n", compPopulacao);
+    printf("Área: CARTA 1 VENCEU? %d \n", compArea);
+    printf("PIB: CARTA 1 VENCEU? %d \n", compPIB);
+    printf("Pontos Turísticos: CARTA 1 VENCEU? %d \n", compPonturi);
+    printf("Densidade Populacional: CARTA 1 VENCEU? %d \n", compDensidade);
+    printf("PIB per Capta: CARTA 1 VENCEU? %d \n", compPIBpercapta);
+    printf("Super Poder: CARTA 1 VENCEU? %d \n", compSuperPoder);
 
     return 0;
 }
